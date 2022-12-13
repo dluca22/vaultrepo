c270da347c7dff6d10c210813d4617111c55f26b
dluca22 <luc.due@tim.it>
2022-12-14 00:54:18 +0100

    moved admin.site model import to its own directory

--- Keymdall/dashboard/admin.py
+++ Keymdall/dashboard/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from vault.models import Login, Folder, History
-from dashboard.models import User
+from .models import User
 
-admin.site.register(Login)
-admin.site.register(Folder)
 admin.site.register(User)
-admin.site.register(History)
--- Keymdall/vault/admin.py
+++ Keymdall/vault/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Login, Folder, History
+
+admin.site.register(Login)
+admin.site.register(Folder)
+admin.site.register(History)
